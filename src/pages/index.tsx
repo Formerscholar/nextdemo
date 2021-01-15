f29cd9641326fd27a6aa64915f7fb2aa227f8a07
Formerscholar <2319315514@qq.com>
2021-01-15 16:30:26 +0800

    redux + thunk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import '../styles/index.less'
+import { useSelector, useDispatch } from 'react-redux'
 import { Button } from 'antd'
+import { startClock } from '../store/actions'
 
 export default function Home() {
+  const state = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(startClock())
+  }, [dispatch])
+
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <code>{JSON.stringify(state, null, 4)}</code>
       <Button type="primary">hello</Button>
     </div>
   )
